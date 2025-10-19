7627cb3136774f0ae36f10b41d45069a8333849c
shreyakj22 <shreyajagadish22@gmail.com>
2025-10-19 14:10:56 +0530

    Add all local project files

---
+++ frontend-donar/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Waste to Need – Donor</h1>
+      <button>Start Donating</button>
+    </div>
+  );
+}
+
+export default App;
